6b2919087b38fc18eaf9a827d86c401576a292f6
Bokstret <Jhonny0900@gmail.com>
2020-06-01 17:03:05 +0300

    Spawner

---
+++ Assets/scripts/Human.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    [SerializeField]
+    Sprite Human1;
+    [SerializeField]
+    Sprite Human2;
+    [SerializeField]
+    Sprite HumanIll;
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 2);
+
+        if (spriteNumber == 1)
+        {
+            spriteRenderer.sprite = Human1;
+        }
+        else
+        {
+            spriteRenderer.sprite = Human2;
+        }
+    }
+
+    public void Initialize(Direction direction, Vector3 position)
+    {
+        
+
+        transform.position = position;
+
+   
+        float angle;
+        float randomAngle = Random.value * 30f * Mathf.Deg2Rad;
+
+        if (direction == Direction.Left)
+        {
+            angle = 165 * Mathf.Deg2Rad + randomAngle;
+        }
+        else
+        {
+            angle = -15 * Mathf.Deg2Rad + randomAngle;
+        }
+
+
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 5f;
+        Vector2 moveDirection = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            moveDirection * magnitude,
+            ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabIll;
+    [SerializeField]
+    GameObject prefabNotIll;
+
+    Vector2 position = new Vector2(0, 0);
+   
+
+    Timer timer;
+    void Start()
+    {
+        
+
+        
+
+        timer = gameObject.AddComponent<Timer>();
+        timer.Duration = 3;
+        timer.Run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer.Finished)
+        {
+            GameObject human = Instantiate<GameObject>(prefabNotIll);
+            CircleCollider2D collider = human.GetComponent<CircleCollider2D>();
+            float humanRadius = collider.radius;
+            Destroy(human);
+
+            float screenHeight = ScreenUtils.ScreenTop - ScreenUtils.ScreenBottom;
+            int illOrNot = Random.Range(0, 3);
+            int side = Random.Range(0, 2);
+            int location = Random.Range(0, 3);
+            
+            if (location == 0)
+            {
+
+                position.y = ScreenUtils.ScreenBottom + screenHeight / 7;
+            }
+            else if (location == 1)
+            {
+
+                position.y = ScreenUtils.ScreenBottom + screenHeight / 2;
+            }
+            else
+            {
+                position.y = ScreenUtils.ScreenBottom + screenHeight / 1.6f;
+            }
+
+            if (side == 0)
+            {
+                //from right to left
+                position.x = ScreenUtils.ScreenLeft - humanRadius;
+                if (illOrNot == 0)
+                {
+                    
+                    human = Instantiate<GameObject>(prefabIll);
+                    Human script = human.GetComponent<Human>();
+                    script.Initialize(Direction.Right, new Vector2(position.x, position.y));
+                }
+                else
+                {
+                    human = Instantiate<GameObject>(prefabNotIll);
+                    Human script = human.GetComponent<Human>();
+                    script.Initialize(Direction.Right, new Vector2(position.x, position.y));
+                }
+            }
+            else
+            {
+                //from left to right
+                position.x = ScreenUtils.ScreenRight + humanRadius;
+                if (illOrNot == 0)
+                {
+                    human = Instantiate<GameObject>(prefabIll);
+                    Human script = human.GetComponent<Human>();
+                    script.Initialize(Direction.Left, new Vector2(position.x, position.y));
+                }
+                else
+                {
+                    human = Instantiate<GameObject>(prefabNotIll);
+                    Human script = human.GetComponent<Human>();
+                    script.Initialize(Direction.Left, new Vector2(position.x, position.y));
+                }
+            }
+            
+
+            timer.Run();
+        }
+    }
+    
+}
